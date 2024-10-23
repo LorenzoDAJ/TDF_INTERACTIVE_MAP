3ea527b33b10560c3ef0486c418515d68f36baaa
LorenzoDAJ <lorenzodaniel.jarata@cvsu.edu.ph>
2024-10-23 13:00:32 +0800

    design updated, delete button utility not implemented

--- src/Pages/Admin/edit/UserManagement.jsx
+++ src/Pages/Admin/edit/UserManagement.jsx
@@ -5,6 +5,7 @@ import { useAuth } from '/src/Pages/Admin/ACMfiles/authContext';
 import styles from  '/src/Pages/Admin/edit/styles/UserManagement.module.scss';  // Import CSS
 import UserModal from './UserModal'; // Component for handling modal input
 
+import icons from "../../../assets/for_landingPage/Icons";
 import NavBar from './navBar/NavBar';
 import AccessBtn from '/src/Pages/Users/landing/signInModule/AccessBtn'; // Import the new AccessBtn component
 import '/src/Pages/Users/landing/signInModule/AccessBtn.module.scss';
@@ -80,37 +81,61 @@ const UserManagement = () => {
         <>
             <NavBar />
             <div>
-                <h2 className={styles.header}>User Management</h2>
-                <button className={styles.btn} onClick={() => openModal()}>Add User</button>
-                <table className={styles.table}>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Password</th>
-                            <th>Role</th>
-                            <th>Created At</th>
-                            <th>Updated At</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {users.map(user => (
-                            <tr key={user._id}>
-                                <td>{user.name}</td>
-                                <td>{user.email}</td>
-                                <td>{user.password}</td>
-                                <td>{user.role}</td>
-                                <td>{new Date(user.createdAt).toLocaleString()}</td>
-                                <td>{new Date(user.updatedAt).toLocaleString()}</td>
-                                <td>
-                                    <button className = {styles.editBtn} onClick={() => openModal(user)}>Edit</button>
-                                    <button className = {styles.delBtn} onClick={() => handleDeleteUser(user._id)}>Delete</button>
-                                </td>
+                <div className = { styles.header }>
+                    <span className = { styles.txtTitle }>User Management</span>
+                </div>
+            
+                <div className = { styles.tblWrapper }>
+                    <div className = { styles.btn }>
+                        <button className={styles.addBtn} onClick={() => openModal()}>Add User</button>
+                    </div>
+                    <table className={styles.table}>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Role</th>
+                                <th>Created At</th>
+                                {/* <th>Updated At</th> */}
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {users.map(user => (
+                                <tr key={user._id}>
+                                    <td>
+                                        <div className = { styles.nameColumn }>
+                                            {/* remove user Icon for now */}
+                                            {/* <div className = { styles.userIcon }>
+                                                <img className = { styles.icon } src = { icons.user } />
+                                            </div> */}
+                                            <div className = { styles.userInfo }>
+                                                <span>{user.name}</span>
+                                                <span className = { styles.email }>{user.email}</span>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <span className = { user?.role === "admin" ? `${ styles.role } ${ styles.admin}` : `${ styles.role } ${ styles.staff}` }>
+                                            {user.role}
+                                        </span>
+                                    </td>
+                                    <td>{new Date(user.createdAt).toLocaleString()}</td>
+                                    {/* <td>{new Date(user.updatedAt).toLocaleString()}</td> */}
+                                    <td>
+                                        <div className = { styles.actionBtns}>
+                                            <button className = {styles.editBtn} onClick={() => openModal(user)}>
+                                                <img className = { `${ styles.icon } ${ styles.pencil}` } src = { icons.pencil } alt = "Edit Item" />
+                                            </button>
+                                            <button className = {styles.delBtn} onClick={() => handleDeleteUser(user._id)}>
+                                                <img className = { `${ styles.icon } ${ styles.delete}` } src = { icons.remove } alt = "Delete Item" />
+                                            </button>
+                                        </div>
+                                    </td>
                                 </tr>
                             ))}
                         </tbody>
                     </table>
+                </div>
                     {modalOpen && <UserModal user={currentUser} onSave={handleAddOrUpdateUser} onClose={() => setModalOpen(false)} />}
                         {/* Button container for absolute positioning */}
                 {/* <div className={styles.accessBtnContainer}>
